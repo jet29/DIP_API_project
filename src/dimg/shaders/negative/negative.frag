1ce8dfbbc8e288d974e8131fb2fa9c2da387a6b5
Frank Ponte <frankponte95@gmail.com>
2019-12-24 11:16:30 -0400

    broken

--- src/dimg/shaders/negative/negative.frag
+++ src/dimg/shaders/negative/negative.frag
@@ -14,5 +14,5 @@ void main(){
 	vec3 negColor = vec3(1.0f-texColor.r,1.0f-texColor.g,1.0f-texColor.b);
 	float grayScale = dot(texColor, vec3(0.2989,0.5870,0.1140));
    // color = vec4(vec3(grayScale),1.0f);
-	color = vec4(negColor,1.0f);
+	color = vec4(vColor,1.0f);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,33 @@
 #include <glm/glm.hpp>
 #include <iostream>
 #include <stb_image.h>
-
-#include "Shader.h"
+#include "dimg/src/dimg.h"
 
 // Window current width
 unsigned int windowWidth = 800;
 // Window current height
 unsigned int windowHeight = 600;
 // Window title
-const char *windowTitle = "Basic Demo";
+const char *windowTitle = "Program to test DIMG";
 // Window pointer
 GLFWwindow *window;
 
 // Shader object
-Shader *shader;
+Shader *shader, *dsShader;
 // Index (GPU) of the geometry buffer
 unsigned int VBO;
 // Index (GPU) vertex array object
 unsigned int VAO;
 // Index (GPU) of the texture
 unsigned int textureID;
+// Deferred shading buffers
+GLuint framebuffer, depthRenderBuffer;
+// Deferred shading texture
+GLuint dsTexture = 0;
+// Images
+GLuint imageID;
+// Digital image processing api
+DIMG api;
 
 /**
  * Handles the window resize
@@ -104,93 +111,67 @@ void initGL()
  * */
 void buildGeometry()
 {
-    float triangleVertices[] = {
-        // Bottom left vertex
-        -0.5f, -0.5f, 0.0f, // Position
-        1.0f, 0.0f, 0.0f,   // Color
-        // Bottom right vertex
-        0.5f, -0.5f, 0.0f, // Position
-        0.0f, 1.0f, 0.0f,  // Color
-        // Top Center vertex
-        0.0f, 0.5f, 0.0f, // Position
-        0.0f, 0.0f, 1.0f  // Color
-
-    };
-    // Creates on GPU the vertex array
-    glGenVertexArrays(1, &VAO);
-    // Creates on GPU the vertex buffer object
-    glGenBuffers(1, &VBO);
-    // Binds the vertex array to set all the its properties
-    glBindVertexArray(VAO);
-    // Sets the buffer geometry data
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), &triangleVertices, GL_STATIC_DRAW);
-
-    // Sets the vertex attributes
-    // Position
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-    // Color
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-    glBindVertexArray(0);
+	// Quad for debug purposes:
+	float quadVertices[] = {
+		// positions        // Color   		   // texture Coords
+		-1.0f,  1.0f, 0.0f, 1.0f, 0.0f, 0.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,  0.0f, 0.0f,
+		 1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+		 1.0f, -1.0f, 0.0f, 0.5f, 0.5f, 0.75f, 1.0f, 0.0f,
+	};
+	// Setup plane VAO
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+	// Position
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	// Color
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	// Texture Coords
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 }
-/**
- * Loads a texture into the GPU
- * @param{const char} path of the texture file
- * @returns{unsigned int} GPU texture index
- * */
-unsigned int loadTexture(const char *path)
-{
-    unsigned int id;
-    // Creates the texture on GPU
-    glGenTextures(1, &id);
-    // Loads the texture
-    int textureWidth, textureHeight, numberOfChannels;
-    // Flips the texture when loads it because in opengl the texture coordinates are flipped
-    stbi_set_flip_vertically_on_load(true);
-    // Loads the texture file data
-    unsigned char *data = stbi_load(path, &textureWidth, &textureHeight, &numberOfChannels, 0);
-    if (data)
-    {
-        // Gets the texture channel format
-        GLenum format;
-        switch (numberOfChannels)
-        {
-        case 1:
-            format = GL_RED;
-            break;
-        case 3:
-            format = GL_RGB;
-            break;
-        case 4:
-            format = GL_RGBA;
-            break;
-        }
 
-        // Binds the texture
-        glBindTexture(GL_TEXTURE_2D, id);
-        // Creates the texture
-        glTexImage2D(GL_TEXTURE_2D, 0, format, textureWidth, textureHeight, 0, format, GL_UNSIGNED_BYTE, data);
-        // Creates the texture mipmaps
-        glGenerateMipmap(GL_TEXTURE_2D);
+bool setFrameBuffer() {
+	glGenFramebuffers(1, &framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 
-        // Set the filtering parameters
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    }
-    else
-    {
-        std::cout << "ERROR:: Unable to load texture " << path << std::endl;
-        glDeleteTextures(1, &id);
-    }
-    // We dont need the data texture anymore because is loaded on the GPU
-    stbi_image_free(data);
+	// The texture we're going to render to
+	glGenTextures(1, &dsTexture);
+	// "Bind" the newly created texture : all future texture functions will modify this texture
+	glBindTexture(GL_TEXTURE_2D, dsTexture);
 
-    return id;
+	// Give an empty image to OpenGL (Which is done with last "0")
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, windowWidth, windowHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	// Depth buffer
+	glGenRenderbuffers(1, &depthRenderBuffer);
+	glBindRenderbuffer(GL_RENDERBUFFER, depthRenderBuffer);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, windowWidth, windowHeight);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderBuffer);
+
+	// Frame buffer configuration
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, dsTexture, 0);
+
+	// Set list of draw buffers
+	GLenum DrawBuffers[1] = { GL_COLOR_ATTACHMENT0 };
+	glDrawBuffers(1, DrawBuffers);
+
+	// Always check that our framebuffer is ok
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		std::cout << "ERROR::Framebuffer configuration went wrong" << std::endl;
+		return false;
+	}
+	return true;
 }
+
 /**
  * Initialize everything
  * @returns{bool} true if everything goes ok
@@ -206,11 +187,12 @@ bool init()
 
     // Loads the shader
     shader = new Shader("assets/shaders/basic.vert", "assets/shaders/basic.frag");
+	dsShader = new Shader("assets/shaders/dsShader.vert", "assets/shaders/dsShader.frag");
     // Loads all the geometry into the GPU
     buildGeometry();
     // Loads the texture into the GPU
-    textureID = loadTexture("assets/textures/bricks2.jpg");
-
+    textureID = api.loadImage("assets/textures/bricks2.jpg");
+	imageID   = api.loadImage("assets/images/zelda.jpg");
     return true;
 }
 /**
@@ -232,6 +214,8 @@ void processKeyboardInput(GLFWwindow *window)
         // Reloads the shader
         delete shader;
         shader = new Shader("assets/shaders/basic.vert", "assets/shaders/basic.frag");
+		delete dsShader;
+		dsShader = new Shader("assets/shaders/dsShader.vert", "assets/shaders/dsShader.frag");
     }
 }
 /**
@@ -239,20 +223,48 @@ void processKeyboardInput(GLFWwindow *window)
  * */
 void render()
 {
-    // Clears the color and depth buffers from the frame buffer
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	///* RENDER NEGATIVE IMAGE TO TEXTURE */
+	//glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+	//// Clears the color and depth buffers from the frame buffer
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    /** Draws code goes here **/
-    // Use the shader
-    shader->use();
-    // Binds the vertex array to be drawn
-    glBindVertexArray(VAO);
-    // Renders the triangle gemotry
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glBindVertexArray(0);
-    // Swap the buffer
-    glfwSwapBuffers(window);
+	///** Draws code goes here **/
+	//// Use the shader
+	//shader->use();
+	//// Send image to GPU
+	//shader->setInt("image", 0);
+	//glActiveTexture(GL_TEXTURE0);
+	//glBindTexture(GL_TEXTURE_2D, imageID);
+	//// Binds the vertex array to be drawn
+	//glBindVertexArray(VAO);
+	//// Renders the triangle gemotry
+	//glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	//glBindVertexArray(0);
+
+	//if (dsTexture == 0)
+	//	dsTexture = api.negative(imageID);
+	//std::cout << "id: " << dsTexture << std::endl;
+
+	api.negative(imageID);
+	/* RENDER NEGATIVE IMAGE */
+	//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//// Use the shader
+	//dsShader->use();
+	//dsShader->setInt("image", 0);
+	//glActiveTexture(GL_TEXTURE0);
+	//glBindTexture(GL_TEXTURE_2D, dsTexture);
+	////Draw QUAD for debug purposes
+	////Binds the vertex array to be drawn
+	//glBindVertexArray(VAO);
+	//// Renders the triangle geometry
+	//glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	//glBindVertexArray(0);
+
+	// Swap the buffer
+	glfwSwapBuffers(window);
 }
+
 /**
  * App main loop
  * */
@@ -266,6 +278,7 @@ void update()
 
         // Renders everything
         render();
+		
 
         // Check and call events
         glfwPollEvents();
@@ -279,8 +292,10 @@ void update()
  * */
 int main(int argc, char const *argv[])
 {
+	// Init api
+	api.setArrayAndBufferObjects(VAO, VBO);
     // Initialize all the app components
-    if (!init())
+    if (!init() /*|| !setFrameBuffer()*/)
     {
         // Something went wrong
         std::cin.ignore();
